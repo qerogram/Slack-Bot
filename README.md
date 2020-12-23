bda4292c9e528c7b3d9d250d6cf5e41868abbeb0
qerogram <qerogram@naver.com>
2020-12-23 13:36:19 +0900

    Update Slack Bot - Just Support PostMessage

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Slack-Bot
+
+## How to Use
+```python
+
+from slack_bot import SlackBot
+
+Bot = SlackBot([Channel_List])
+Bot.sendMessage("Messsage")
+```
---
+++ slack_bot.py
@@ -0,0 +1,67 @@
+# Author : Qerogram
+import requests, json
+
+class SlackBot() :
+    headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    params = {
+
+    }
+
+    Id_Of_Channels = {}
+
+    def __init__(self, channel) :
+        self.channels = channel
+        self.slack_token = self.get_token()
+        self.headers['Authorization'] = f"Bearer {self.slack_token}"
+        self.params["token"] = self.slack_token
+        if self.isExistChannel(self.channels) == 0 :
+            return
+
+        print("[+] Success : Find Channels")        
+
+        self.getChannelList()
+
+    def get_token(self) :
+        return open("token.txt","r").read()
+    
+    def getChannelList(self) :
+        print("\n" + "=" * 30)
+        for name, id in self.Id_Of_Channels.items() :
+            print(f"  [-] {name} : {id}")
+        print("=" * 30 + "\n")
+
+    def isExistChannel(self, channels) :
+        URL = "https://slack.com/api/conversations.list"
+        res = requests.get(URL, params=self.params, headers=self.headers)
+
+        response = res.json()
+
+        for element in response['channels'] :
+            for channel in channels :
+                if element['name'] == channel :
+                    self.Id_Of_Channels[element['name']] = element['id']
+
+        return len(channels)
+    
+    def sendMessage(self, Message) :
+        URL = "https://slack.com/api/chat.postMessage"
+
+        for Channel in self.Id_Of_Channels.values() :
+            data = self.params
+            data['channel'] = Channel
+            data['text'] = Message
+            res = requests.post(URL, data=data, headers=self.headers)
+
+            if res.json()['ok'] != False : 
+                print(f"[+] Success : Send Message({Message}) to {Channel}")
+            else :
+                print(f"[+] Fail : Send Message({Message}) to {Channel}")
+
+if __name__ == "__main__" :
+    # Example : Constructor(Slack Channel)
+    # if occured not_in_channel, you invite bot app in channel like @botname
+    Bot = SlackBot(['hack_news'])
+    Bot.sendMessage("testMessage")
